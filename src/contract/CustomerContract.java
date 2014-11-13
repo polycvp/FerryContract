f10d3609bc4d8945c0bcfee56d656aa5f1a06ff2
Paul <paul@sensus.dk>
2014-11-13 10:42:39 +0100

    skeleton

---
+++ src/contract/CustomerContract.java
@@ -0,0 +1,15 @@
+/*
+ * This class contains the methods that need to be implemented to connect the frontend and backend of the ferry application
+    Provide javadoc descriptions to each interface method
+    Define necessary Data Transfer Objects (dto) and Exception Transfer Objects (eto) for each method in the specific packages
+ */
+
+package contract;
+
+/**
+ *
+ * @author Group F
+ */
+public interface CustomerContract {
+    
+}
